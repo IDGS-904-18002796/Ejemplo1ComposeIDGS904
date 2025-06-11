c5051e12a95d183ecc034520026c2aa22faf9229
IDGS-904-18002796 <18002796@alumnos.utleon.edu.mx>
2025-06-10 23:15:28 -0600

    Terjetas Terminadas 10/06/2025

--- app/src/main/java/com/jsalas/ejemplo1composeidgs904/MainActivity.kt
+++ app/src/main/java/com/jsalas/ejemplo1composeidgs904/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.jsalas.ejemplo1composeidgs904
 
-import android.media.Image
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
@@ -12,66 +12,126 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.jsalas.ejemplo1composeidgs904.ui.theme.Ejemplo1ComposeIDGS904Theme
+import androidx.compose.foundation.lazy.items
+
+private val tarjetas: List<PersonajeTarjeta> = listOf(
+    PersonajeTarjeta("Goku", "El protagonista de la serie, conocido por su gran poder y personalidad amigable. Originalmente enviado a la Tierra como un infante volador con la misión de conquistarla. Sin embargo, el caer por un barranco le proporcionó un brutal golpe que si bien casi lo mata, este alteró su memoria y anuló todos los instintos violentos de su especie, lo que lo hizo crecer con un corazón puro y bondadoso, pero conservando todos los poderes de su raza."),
+    PersonajeTarjeta("Vegeta", "Príncipe de los Saiyans, inicialmente un villano, pero luego se une a los Z Fighters. A pesar de que a inicios de Dragon Ball Z, Vegeta cumple un papel antagónico, poco después decide rebelarse ante el Imperio de Freeza, volviéndose un aliado clave para los Guerreros Z."),
+    PersonajeTarjeta("Picolo", "Es un namekiano que surgió tras ser creado en los últimos momentos de vida de su padre, siendo su actual reencarnación. Aunque en un principio fue el archienemigo de Son Goku, con el paso del tiempo fue haciéndose menos malvado hasta finalmente convertirse en un ser bondadoso y miembro de los Guerreros Z."),
+    PersonajeTarjeta("Bulma", "Bulma es la protagonista femenina de la serie manga Dragon Ball y sus adaptaciones al anime Dragon Ball, Dragon Ball Z, Dragon Ball Super y Dragon Ball GT. Es hija del Dr. Brief y su esposa Panchy, hermana menor de Tights y una gran amiga de Son Goku con quien inicia la búsqueda de las Esferas del Dragón."),
+    PersonajeTarjeta("Freezer", "Freezer es el tirano espacial y el principal antagonista de la saga de Freezer."),
+    PersonajeTarjeta("Zarbon", "Zarbon es uno de los secuaces de Freezer y un luchador poderoso."),
+    PersonajeTarjeta("Dodoria", "Dodoria es otro secuaz de Freezer conocido por su brutalidad."),
+    PersonajeTarjeta("Ginyu", "Ginyu es el líder del la élite de mercenarios de mayor prestigio del Ejército de Freeza, la cual lleva el nombre de Fuerzas Especiales Ginew"),
+    PersonajeTarjeta("Celula", "Cell conocido como Célula en España, es un bioandroide creado por la computadora del Dr. Gero, quien vino del futuro de la línea 3 con la intención de vengarse de Goku por haber acabado con el Ejército del Listón Rojo, y con ello el sueño de todo villano: dominar el mundo. Es el antagonista principal del Arco de los Androides y Cell."),
+    PersonajeTarjeta("Gohan", "Son Gohanda en su tiempo en España, o simplemente Gohan en Hispanoamérica, es uno de los personajes principales de los arcos argumentales de Dragon Ball Z, Dragon Ball Super y Dragon Ball GT. Es un mestizo entre saiyano y humano terrícola. Es el primer hijo de Son Goku y Chi-Chi, hermano mayor de Son Goten, esposo de Videl y padre de Pan.")
+
+);
+
+data class PersonajeTarjeta(val title: String, val body: String)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Ejemplo1ComposeIDGS904Theme {
-                Tarjeta()
-            }
 
+
+        setContent {
+            AppTheme  {
+
+                Tarjeta(tarjetas)
+            }
         }
     }
-}
-data class Personaje(val nombre:String,val descripcion:String)
-@Composable
-fun Tarjeta(){
-    Row(modifier = Modifier.padding(8.dp)
-        .background(MaterialTheme.colorScheme.background)) {
-        ImagenHeroe()
-        Personaje()
+
+    @Composable
+    fun Tarjeta(personajes : List<PersonajeTarjeta>) {
+        LazyColumn {
+            items (personajes){
+                    personaje -> MyPersonajes(personaje)
+            }
+        }
 
     }
 
-}
 
-@Composable
-fun Personaje() {
-    Column {
-    Text("Nombre: Goku")
-    Text("Descripcion: El protagonista de la serie, conocido por su gran poder y personalidad amigable. Originalmente enviado a la Tierra como un infante volador con la misión de conquistarla. Sin embargo, el caer por un barranco le proporcionó un brutal golpe que si bien casi lo mata, este alteró su memoria y anuló todos los instintos violentos de su especie, lo que lo hizo crecer con un corazón puro y bondadoso, pero conservando todos los poderes de su raza. No obstante, en la nueva continuidad de Dragon Ball se establece que él fue enviado por sus padres a la Tierra con el objetivo de sobrevivir a toda costa a la destrucción de su planeta por parte de Freeza. Más tarde, Kakarot, ahora conocido como Son Goku, se convertiría en el príncipe consorte del monte Fry-pan y líder de los Guerreros Z, así como el mayor defensor de la Tierra y del Universo 7, logrando mantenerlos a salvo de la destrucción en innumerables ocasiones, a pesar de no considerarse a sí mismo como un héroe o salvador.")
-}
-}
+    @Preview
+    @Composable
+    fun PreviewMessageCard() {
+        Tarjeta(tarjetas)
+    }
 
-@Composable
-fun ImagenHeroe(){
-    Image(
-        painterResource(id=R.drawable.goku_normal),
-        contentDescription="Goku",
-        modifier = Modifier
-            .padding(16.dp)
-            .size(100.dp)
-            .clip(CircleShape)
-            .background(MaterialTheme.colorScheme.primary)
-    )
-}
+    @Composable
+    fun MyPersonajes(personaje:PersonajeTarjeta){
 
-@Preview
-@Composable
-fun PreviewMessageCard(){
-Tarjeta()
+        Card (
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxSize(),
+            elevation = CardDefaults.cardElevation(defaultElevation = 8.dp ),
+            shape = RoundedCornerShape(16.dp),
+            colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface)
+        ){
+            Row(
+                modifier = Modifier
+                    .background(MaterialTheme.colorScheme.background)
+                    .padding(8.dp)
+            ){
+                ImagenHeroe(personaje.title.lowercase())
+                Personajes(personaje)
+            }
+        }
+    }
+
+    @Composable
+    fun Personajes(personaje: PersonajeTarjeta){
+        Column{
+            Personaje(personaje.title,
+                MaterialTheme.colorScheme.primary,
+                MaterialTheme.typography.titleLarge)
+            Personaje(personaje.body,
+                MaterialTheme.colorScheme.primary,
+                MaterialTheme.typography.bodyMedium)
+        }
+    }
+
+
+    @Composable
+    fun Personaje(name:String, color: Color, style: TextStyle){
+        Text(text = name, color = color, style = style)
+    }
+
+    @Composable
+    fun ImagenHeroe(imageName:String){
+        val context = LocalContext.current
+        val imageResid = remember (imageName) {
+            context.resources.getIdentifier(imageName.lowercase(),
+                "drawable", context.packageName)
+        }
+        Image(
+            painterResource(id = imageResid),
+            contentDescription = imageName,
+            modifier = Modifier
+                .padding(16.dp)
+                .size(100.dp)
+                .clip(CircleShape)
+                .background(MaterialTheme.colorScheme.primary)
+
+        )
+    }
+
 }
